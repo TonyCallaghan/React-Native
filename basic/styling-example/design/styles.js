aa86ba9195757599da912f0e05e029b1569220f0
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2023-07-04 22:51:02 +0100

    Create styles.js

---
+++ basic/styling-example/design/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      text: {
+        backgroundColor: '#FDE2F3',
+        color: '#917FB3',
+        fontFamily: 'Helvetica',
+        fontSize: 40,
+        padding: 10,
+        margin: 10,
+        borderRadius: 25,
+        overflow: 'hidden',
+        fontStyle: 'italic',
+      },
+  });
